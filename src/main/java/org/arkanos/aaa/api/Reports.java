43780e39602b7a84b73dad88c20e0854f4dc266b
Matheus Borges Teixeira <matheus.bt@gmail.com>
2015-08-11 00:31:38 +0200

    Compiling JSON data and display for arrow counts.

--- src/main/java/org/arkanos/aaa/api/Reports.java
+++ src/main/java/org/arkanos/aaa/api/Reports.java
@@ -1,16 +1,21 @@
 package org.arkanos.aaa.api;
 
 import java.io.IOException;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
+import java.util.HashMap;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.arkanos.aaa.controllers.Database;
 
 /**
  * Servlet implementation class Reports
@@ -24,6 +29,7 @@ public class Reports extends HttpServlet {
      */
     public Reports() {
         super();
+        Database.initialize();
         // TODO Auto-generated constructor stub
     }
 
@@ -74,18 +80,112 @@ public class Reports extends HttpServlet {
 			gc.set(Calendar.WEEK_OF_YEAR,week_end+1);
 			Date end = gc.getTime();
 					
-			SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+			SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
 			
-			System.out.println(format.format(start));
-			System.out.println(format.format(end));
+			System.out.println(sdf.format(start));
+			System.out.println(sdf.format(end));
+			
+			HashMap<Integer,HashMap<String,HashMap<String,Integer>>> distance_type = new HashMap<Integer,HashMap<String,HashMap<String,Integer>>>();
+			HashMap<String,HashMap<String,Integer>> technique_days = new HashMap<String,HashMap<String,Integer>>();
+			try{
+				
+				//SELECT SUM(arrows) as arrows,date,type FROM aaa.training_technique WHERE archer='arkanos' AND type != 'target' AND date >= '2015-07-27' AND date < '2015-09-07' GROUP BY date,type;
+				ResultSet rs = Database.query("SELECT SUM(arrows) as arrows,date,type FROM aaa.training_technique WHERE archer='arkanos' AND type != 'target' AND date >= '"+sdf.format(start)+"' AND date < '"+sdf.format(end)+"' GROUP BY date,type;");
+				while(rs.next()){
+					HashMap<String,Integer> parent = technique_days.get(rs.getString("type"));
+					if(parent == null){
+						parent = new HashMap<String,Integer>();
+						technique_days.put(rs.getString("type"),parent);
+					}
+					parent.put(sdf.format(rs.getDate("date")), rs.getInt("arrows"));
+				}
+				rs.close();
+				
+				//SELECT SUM(arrows) as arrows,distance,date,type FROM aaa.training_technique WHERE archer='arkanos' AND date >= '2015-07-27' AND date < '2015-09-07' GROUP BY distance,date,type;
+				rs = Database.query("SELECT SUM(arrows) as arrows,distance,date,type FROM aaa.training_technique WHERE archer='arkanos' AND type = 'target' AND date >= '"+sdf.format(start)+"' AND date < '"+sdf.format(end)+"' GROUP BY distance,date,type;");
+				while(rs.next()){
+					HashMap<String,HashMap<String,Integer>> grandparent = distance_type.get(rs.getInt("distance"));
+					if(grandparent == null){
+						grandparent = new HashMap<String,HashMap<String,Integer>>();
+						distance_type.put(rs.getInt("distance"),grandparent);
+					}
+					HashMap<String,Integer> parent = grandparent.get(rs.getString("type"));
+					if(parent == null){
+						parent = new HashMap<String,Integer>();
+						grandparent.put(rs.getString("type"),parent);
+					}
+					parent.put(sdf.format(rs.getDate("date")), rs.getInt("arrows"));
+				}
+				rs.close();
+				
+				//SELECT COUNT(value) as total_arrows,date,distance FROM arrow LEFT JOIN training_target ON id = training_id WHERE archer='arkanos' GROUP BY date,distance;
+				rs = Database.query("SELECT COUNT(value) as arrows,date,distance FROM arrow LEFT JOIN training_target ON id = training_id WHERE archer='arkanos' AND date >= '"+sdf.format(start)+"' AND date < '"+sdf.format(end)+"' GROUP BY date,distance;");
+				while(rs.next()){
+					HashMap<String,HashMap<String,Integer>> grandparent = distance_type.get(rs.getInt("distance"));
+					if(grandparent == null){
+						grandparent = new HashMap<String,HashMap<String,Integer>>();
+						distance_type.put(rs.getInt("distance"),grandparent);
+					}
+					HashMap<String,Integer> parent = grandparent.get("gauged");
+					if(parent == null){
+						parent = new HashMap<String,Integer>();
+						grandparent.put("gauged",parent);
+					}
+					parent.put(sdf.format(rs.getDate("date")), rs.getInt("arrows"));
+				}
+				rs.close();
+				
+				
+			}
+			catch(SQLException e){
+				//TODO
+				e.printStackTrace();
+			}
+			
+			
 			
 			json += "{";
-			json += "\"start\":\""+format.format(start)+"\",";
-			json += "\"end\":\""+format.format(end)+"\",";
+			
+			json += "\"arrow_counts\":";
+
+			json += "{";
+			for(String t: technique_days.keySet()){
+				json += "\""+t+"\":";
+				json += "{";
+				for(String d: technique_days.get(t).keySet()){
+					json += "\""+d+"\":"+technique_days.get(t).get(d)+",";
+				}
+				if(json.endsWith(",")) json = json.substring(0,json.lastIndexOf(','));
+				json += "},";
+			}
+			
+			for(Integer di: distance_type.keySet()){
+				json += "\""+di+"\":";
+				json += "{";
+				for(String t: distance_type.get(di).keySet()){
+					json += "\""+t+"\":";
+					json += "{";
+					for(String d: distance_type.get(di).get(t).keySet()){
+						json += "\""+d+"\":"+distance_type.get(di).get(t).get(d)+",";
+					}
+					if(json.endsWith(",")) json = json.substring(0,json.lastIndexOf(','));
+					json += "},";
+				}
+				if(json.endsWith(",")) json = json.substring(0,json.lastIndexOf(','));
+				json += "},";
+			}
+			
+			if(json.endsWith(",")) json = json.substring(0,json.lastIndexOf(','));
+			
+			json += "},";
+			
+			json += "\"start\":\""+sdf.format(start)+"\",";
+			json += "\"end\":\""+sdf.format(end)+"\",";
 			json += "\"week_start\":"+week_start+",";
 			json += "\"week_end\":"+week_end+",";
+			json += "\"month\":"+month+",";
 			json += "\"response\":200}";
-			
+			System.out.println(json);
 		}
 		//TODO throw error, treat exceptions
 		
--- src/main/webapp/scripts/reporting.js
+++ src/main/webapp/scripts/reporting.js
@@ -17,31 +17,84 @@ xmlhttp.send();
 function buildMonthlyReport(download){
 	var week = download.week_start;
 	
+	var request = download.month;
 	var current = new Date(download.start);
 	var stop = new Date(download.end);
 	
-	var width = 100/((stop - current)/(1000*60*60*24*7))
+	var width = 85/((stop - current)/(1000*60*60*24*7))
 	var html = "<div>";
+
+	html += "<div class='sidebar'>";
+	html += "<div class='skip_header'></div>";
+	html += "<div id='labels'>";
+	
+	html += "</div>";
+	html += "</div>";
 	
 	while(current < stop){
 		html += "<div class='week' style='width:"+width+"%'>";
-
+		
 		html += "<div class='week-title'>"
 		html += "<h2>"+week+"</h2><p>Wochen-Nr.</p>";
 		html += "</div>";
-		
+
 		html += "<div class='days'>"
 		for(i = 0; i < 7;i++){
-			html += "<div class='day'>";
+			html += "<div class='day header_day'>";
 			html += current.getDate();
 			html += "</div>";
 			current.setDate(current.getDate() + 1);
 			console.log(current);
 		}
+		html += "<div id='week-data-"+week+"'></div>";
+		
+		html += "</div>";
+		
+		html += "</div>";
 		week++;
-		html += "</div>";
-		html += "</div>";
 	}
 	html += "</div>"
 	document.body.innerHTML = html;
+	
+	
+	for(var distance in download.arrow_counts){
+		
+		if(distance != 'warmup' && distance != 'warmout'){
+			html = document.getElementById("labels").innerHTML;
+			html += "<div id='counts-"+distance+"' class='label'>"
+			html += "<p>"+distance+" m</p>";
+			html += "</div>";
+			document.getElementById("labels").innerHTML = html;
+			for(var type in download.arrow_counts[distance]){
+				
+				html = document.getElementById("counts-"+distance).innerHTML;
+				html += "<div>"+type+"</div>";
+				document.getElementById("counts-"+distance).innerHTML = html;
+				
+				current = new Date(download.start);
+				week = download.week_start;
+				while(current < stop){
+					html = document.getElementById("week-data-"+week).innerHTML;
+					for(i = 0; i < 7;i++){
+						html += "<div class='day";
+						if(i > 4) html += " weekend";
+						if(current.getMonth() + 1 != download.month) html += " offtime";
+						html += "'>";
+						if(download.arrow_counts[distance][type][current.toJSON().substring(0,10)]){
+							html += download.arrow_counts[distance][type][current.toJSON().substring(0,10)];
+						}
+						else{
+							html += " ";
+						}
+						html += "</div>";
+						current.setDate(current.getDate() + 1);
+					}
+					console.log(week+","+current);
+					document.getElementById("week-data-"+week).innerHTML = html;
+					week++;				
+				}
+			}
+		}
+	}
+	
 }
