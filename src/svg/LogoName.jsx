7b568cbc6f054d86ebba5ebaf7810380c11f8eff
Matheus Borges Teixeira <matheus.bt@gmail.com>
2017-04-18 22:44:18 +0200

    Creating logo with and without name.

---
+++ src/svg/LogoName.jsx
@@ -0,0 +1,27 @@
+const React = require('react');
+
+const i18nextReact = require('global/i18nextReact');
+
+const Logo = require('svg/Logo');
+
+const LogoName = React.createClass({
+  render: function() {
+    const t = this.props.t;
+    return (
+      <svg width={this.props.width} height={this.props.height} viewBox="0 0 100 100" >
+        <g transform={'translate(-140,0)'}><Logo /></g>
+    		<text y={83} x={50} style={{
+            fontSize: 24,
+            textAnchor: 'middle',
+            fontFamily: 'Arial',
+            fontWeight: 'bold',
+            fill: '#996666'
+          }}>
+    			{t('common:appTitle')}
+    		</text>
+      </svg>
+    );
+  }
+});
+
+module.exports = i18nextReact.setupTranslation(['common'], LogoName);
