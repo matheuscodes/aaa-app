d30f9a27e15798c000cedb1e3c9d8c4004471c01
Matheus Borges Teixeira <matheus.bt@gmail.com>
2020-02-27 21:04:27 +0100

    Bumping version.

--- src/app/common/Footer.jsx
+++ src/app/common/Footer.jsx
@@ -172,7 +172,7 @@ var Footer = React.createClass({
         </div>
         <div style={this.style.copyright.div}>
           <span style={this.style.copyright.span}>
-            Matheus Borges Teixeira &copy; 2020 - Version 2.0.1
+            Matheus Borges Teixeira &copy; 2020 - Version 2.1.0
           </span>
         </div>
       </footer>
