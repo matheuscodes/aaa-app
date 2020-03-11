a9b42b43ec796011dae63412c65a40929105097c
Matheus Borges Teixeira <matheus.bt@gmail.com>
2020-03-12 00:03:16 +0100

    Starting point with react + material ui and scripts.

---
+++ src/Application.js
@@ -0,0 +1,88 @@
+
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import Link from '@material-ui/core/Link';
+import ProTip from './ProTip';
+
+import React, { Component, Suspense } from 'react';
+import { useTranslation, withTranslation, Trans } from 'react-i18next';
+
+
+// use hoc for class based components
+class LegacyWelcomeClass extends Component {
+  render() {
+    const { t } = this.props;
+    return <h2>{t('common:appTitle')}</h2>;
+  }
+}
+const Welcome = withTranslation()(LegacyWelcomeClass);
+
+// Component using the Trans component
+function MyComponent() {
+  return (
+    <Trans i18nKey="common:logout">
+      To get started, edit <code>src/App.js</code> and save to reload.
+    </Trans>
+  );
+}
+
+// page uses the hook
+function Page() {
+  const { t, i18n } = useTranslation();
+
+  const changeLanguage = lng => {
+    i18n.changeLanguage(lng);
+  };
+
+  return (
+    <div className="App">
+      <div className="App-header">
+        <Welcome />
+        <button onClick={() => changeLanguage('de')}>de</button>
+        <button onClick={() => changeLanguage('en')}>en</button>
+      </div>
+      <div className="App-intro">
+        <MyComponent />
+      </div>
+      <div>{t('common:logout')}</div>
+    </div>
+  );
+}
+
+// loading component for suspense fallback
+const Loader = () => (
+  <div className="App">
+    <div>loading...</div>
+  </div>
+);
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="https://material-ui.com/">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+export default function App() {
+  return (
+    <Container maxWidth="sm">
+    <Suspense fallback={<Loader />}>
+    <Page />
+  </Suspense>
+      <Box my={4}>
+        <Typography variant="h4" component="h1" gutterBottom>
+          Create React App v4-beta example
+        </Typography>
+        <ProTip />
+        <Copyright />
+      </Box>
+    </Container>
+  );
+}
---
+++ src/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { ThemeProvider } from '@material-ui/core/styles';
+// import './index.css';
+
+// import i18n (needs to be bundled ;))
+import './i18n';
+import App from './Application';
+import theme from './theme';
+
+ReactDOM.render(<ThemeProvider theme={theme}>
+    {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+    <CssBaseline />
+    <App />
+  </ThemeProvider>, document.getElementById('root'));
