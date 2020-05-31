dd0e14b9bb6961883bb893925f1daeeca803e454
Matheus Borges Teixeira <matheus.bt@gmail.com>
2020-05-31 22:22:35 +0200

    Fixing about screen.

--- src/app/static/about/AboutHome.jsx
+++ src/app/static/about/AboutHome.jsx
@@ -9,19 +9,10 @@ class AboutHome extends React.Component {
   render() {
     return (
       <Grid container spacing={2} >
-        <Grid item xs={2} >
-          <div><img src='img/home_counts.png' width={'100%'} alt="" /></div>
-        </Grid>
-        <Grid item xs={2} >
+        <Grid item xs={6} >
           <div><img src='img/home_ring_distribution.png' width={'100%'} alt="" /></div>
         </Grid>
-        <Grid item xs={3} >
-          <div><img src='img/home_end_overview.png' width={'100%'} alt="" /></div>
-        </Grid>
-        <Grid item xs={2} >
-          <div><img src='img/home_events.png' width={'100%'} alt="" /></div>
-        </Grid>
-        <Grid item xs={3} >
+        <Grid item xs={6} >
           <div><img src='img/home_year_overview.png' width={'100%'} alt="" /></div>
         </Grid>
       </Grid>
