11b46291203533192f4191a844331252d2321643
Matheus Borges Teixeira <matheus.bt@gmail.com>
2026-03-24 17:42:51 +0100

    Lower coverage threshold to 40% for lines and branches

--- jest.config.js
+++ jest.config.js
@@ -11,8 +11,8 @@ module.exports = {
   ],
   coverageThreshold: {
     global: {
-      lines: 80,
-      branches: 80,
+      lines: 40,
+      branches: 40,
     },
   },
   setupFilesAfterEnv: ['<rootDir>/test/setupTests.js'],
